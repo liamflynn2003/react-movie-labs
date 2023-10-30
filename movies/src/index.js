23c6fd59503050a12754ccdae41b2fbd1c6d33e8
liamflynn2003 <20098690@mail.wit.ie>
2023-10-30 15:22:15 +0000

    Change site header.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,22 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/movies/favorites">Favorites</Link>
-        </li>
-      </ul>
+          <BrowserRouter>
+      <SiteHeader />
+      <Routes>
+        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+        <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+        <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={ <Navigate to="/" /> } />
+      </Routes>
+    </BrowserRouter>
       <Routes>
       <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
         <Route exact path="/movies/favorites" element={<FavoriteMoviesPage />} />
